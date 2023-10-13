0c5f53ca96a3dfe780f4bcc33ea44923138cfc06
Alec Schram <alecschram@hotmail.com>
2023-10-13 14:07:28 -0400

    Removed colon from SubDoctrine title.

--- src/view/DoctrineScreens/FirstSubDoctrineScreen.js
+++ src/view/DoctrineScreens/FirstSubDoctrineScreen.js
@@ -40,14 +40,14 @@ const FirstSubDoctrine = ({ route, navigation }) => {
                 {item.Reference.map((item, index) => {
                 return (
                     <View key={index}>
-                        <Text style={styles.topicStyle}>{item.Topic}:</Text>
+                        <Text style={styles.topicStyle}>{item.Topic}</Text>
                         {item.Verses &&
                             <Text style={styles.verseStyle}>{item.Verses.join("|").toString().split("|").join("\n")}</Text>}
                         {item.Reference && <>
                         {item.Reference.map((item, index) => {
                             return(
                             <View key={index}>
-                                <Text style={styles.subTopicStyle}>{item.SubTopic}:</Text>
+                                <Text style={styles.subTopicStyle}>{item.SubTopic}</Text>
                                 {item.Verses &&
                                 <Text style={styles.subVerseStyle}>{item.Verses.join("|").toString().split("|").join("\n")}</Text>}
                             </View>
